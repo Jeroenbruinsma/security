0b62994af55530172ecd67ca8375fdae9b818e65
Jeroen <Jeroenbruinsma@gmail.com>
2020-06-25 13:52:43 +0200

    add a user

--- routers/image.js
+++ routers/image.js
@@ -1,27 +1,26 @@
 const {Router} = require("express")
 const Image = require("../models").image
 
-
 const router = new Router()
 
-router.get("/", (request, response) =>{
+router.get("/", async (request, response) =>{
     console.log("Got a request on /image", request.body)
-    response.send("it works for image")
+    const limit = Math.min( [request.query.limit, 30  ])
+    const offset = request.query.offset  
+    const images = await Image.findAll({limit, offset})
+    response.json({images})
 } )
 
 router.post("/", async (request, response) =>{
     console.log("Got a post request on /image", request.body)
     try{
         const demoImg = await Image.create({title: "some demo title", url: "someurl.com/mountain.png"})
-        console.log("what is demoImg", demoImg)
         response.send("it works for image")
     }
     catch(err){
         console.log("err", err)
         response.send("inserting img didn't")
     }
-
 } )
-
 
 module.exports =  router
--- routers/user.js
+++ routers/user.js
@@ -1,11 +1,36 @@
-const {Router} = require("express")
-
+const {Router, request, response} = require("express")
 const router = new Router()
+const User = require("../models").user
 
 router.get("/", (request, response) =>{
     console.log("Got a request on /user", request.body)
     response.send("it also works for users")
 } )
+
+
+router.post("/", async (request, response) => {
+    console.log("Got a post request on /user", request.body)
+    //prevent this from happening when there is no password or name or.. .....
+    const {email, password, fullName} = request.body
+    if(!email || !password || !fullName){
+        return response.send("missing some info! please supply email, pass, name")
+    }
+    try{
+        const newUser = await User.create({email, password, fullName}) 
+        console.log("the new user:", newUser.dataValues)
+
+        const user =  {...newUser.dataValues}
+        delete user.password
+        response.json(user)
+
+    }catch(err){
+        response.send("something went wrong")
+    }
+
+    
+
+
+}) 
 
 
 module.exports =  router
